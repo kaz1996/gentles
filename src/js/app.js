06dd04af7ea8d70e859e774f5a4f91275ec2ca8f
kazu_nk <kaznobe920@outlook.com>
2020-01-13 13:53:56 +0900

    Add filter function

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,32 @@
 import '../scss/main.scss';
+
+const filterButtons = document.querySelectorAll('.filter__button');
+const cards = document.querySelectorAll('.card');
+
+const filterCards = (buttonText) => {
+  cards.forEach((card) => {
+    if (buttonText === 'all') {
+      card.classList.remove('card--display-none');
+    } else if (buttonText === card.dataset.tag) {
+      card.classList.remove('card--display-none');
+    } else {
+      card.classList.add('card--display-none');
+    }
+  });
+};
+
+const removeActiveClass = () => {
+  filterButtons.forEach((filterButton) => {
+    if (filterButton.classList.contains('button--active')) {
+      filterButton.classList.remove('button--active');
+    }
+  });
+};
+
+filterButtons.forEach((filterButton) => {
+  filterButton.addEventListener('click', () => {
+    removeActiveClass();
+    filterCards(filterButton.textContent.toLocaleLowerCase());
+    filterButton.classList.add('button--active');
+  });
+});
